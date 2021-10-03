dbbf47863ef15420481b0669f0cd37ed4082ccaf
TheoCaldas <theocaldas28@gmail.com>
2021-10-02 22:01:47 -0300

    add lauch screen image and bottom navigation bar

---
+++ covidmonitor/lib/constants.dart
@@ -0,0 +1,6 @@
+import 'package:flutter/material.dart';
+
+class Constants {
+  static final Color backgroundColor = Color.fromARGB(255, 124, 209, 176);
+  static final Color primaryColor = Color.fromARGB(255, 255, 255, 255);
+}
--- covidmonitor/lib/main.dart
+++ covidmonitor/lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:http/http.dart' as http;
 import 'states.dart';
+import 'constants.dart';
 
 void main() {
   runApp(MyApp());
@@ -42,7 +43,7 @@ class _MyHomePageState extends State<MyHomePage> {
         });
     if (response.statusCode == 200) {
       final statesResult = statesResultFromJson(response.body);
-      debugStateResult(statesResult);
+      //debugStateResult(statesResult);
       return statesResult;
     } else {
       print("statusCode: " + response.statusCode.toString());
@@ -71,45 +72,110 @@ class _MyHomePageState extends State<MyHomePage> {
     super.initState();
   }
 
+  int _selectedIndex = 0;
+
+  // static const TextStyle optionStyle =
+  //     TextStyle(fontSize: 30, fontWeight: FontWeight.bold);
+  // static const List<Widget> _widgetOptions = <Widget>[
+  //   Text(
+  //     'Index 0: Home',
+  //     style: optionStyle,
+  //   ),
+  //   Text(
+  //     'Index 1: Business',
+  //     style: optionStyle,
+  //   ),
+  //   Text(
+  //     'Index 2: School',
+  //     style: optionStyle,
+  //   ),
+  //   Text(
+  //     'Index 3: Settings',
+  //     style: optionStyle,
+  //   ),
+  // ];
+
+  void _onItemTapped(int index) {
+    setState(() {
+      _selectedIndex = index;
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        appBar: AppBar(
-          title: Text(widget.title),
-        ),
-        body: FutureBuilder<StatesResult>(
-          future: _statesResult,
-          builder: (context, snapshot) {
-            if (snapshot.hasData) {
-              return ListView.builder(
-                  padding: const EdgeInsets.all(30),
-                  itemCount: snapshot.data!.results.length,
-                  itemBuilder: (BuildContext context, int index) {
-                    return Card(
-                        child: Center(
-                      child: Row(
-                        mainAxisSize: MainAxisSize.min,
-                        children: <Widget>[
-                          Text(
-                            snapshot.data!.results[index].state,
-                            style: TextStyle(fontWeight: FontWeight.bold),
-                          ),
-                          Padding(padding: EdgeInsets.all(30)),
-                          Text(
-                            "Casos confirmados: " +
-                                snapshot.data!.results[index].confirmed
-                                    .toString(),
-                            style: TextStyle(fontWeight: FontWeight.normal),
-                          ),
-                        ],
-                      ),
-                    ));
-                  });
-            } else if (snapshot.hasError) {
-              return Text('${snapshot.error}');
-            }
-            return Center(child: CircularProgressIndicator());
-          },
-        ));
+      appBar: AppBar(
+        title: Text(widget.title),
+        backgroundColor: Constants.backgroundColor,
+      ),
+      body: FutureBuilder<StatesResult>(
+        future: _statesResult,
+        builder: (context, snapshot) {
+          if (snapshot.hasData) {
+            return ListView.builder(
+                padding: const EdgeInsets.all(30),
+                itemCount: snapshot.data!.results.length,
+                itemBuilder: (BuildContext context, int index) {
+                  return Card(
+                      color: Constants.primaryColor,
+                      child: Center(
+                        child: Row(
+                          mainAxisSize: MainAxisSize.min,
+                          children: <Widget>[
+                            Text(
+                              snapshot.data!.results[index].state,
+                              style: TextStyle(
+                                fontWeight: FontWeight.bold,
+                                color: Constants.backgroundColor,
+                              ),
+                            ),
+                            Padding(padding: EdgeInsets.all(30)),
+                            Text(
+                              "Casos confirmados: " +
+                                  snapshot.data!.results[index].confirmed
+                                      .toString(),
+                              style: TextStyle(
+                                fontWeight: FontWeight.normal,
+                                color: Constants.backgroundColor,
+                              ),
+                            ),
+                          ],
+                        ),
+                      ));
+                });
+          } else if (snapshot.hasError) {
+            return Text('${snapshot.error}');
+          }
+          return Center(child: CircularProgressIndicator());
+        },
+      ),
+      bottomNavigationBar: BottomNavigationBar(
+        items: <BottomNavigationBarItem>[
+          BottomNavigationBarItem(
+            icon: Icon(Icons.home),
+            label: 'Home',
+            backgroundColor: Constants.backgroundColor,
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.business),
+            label: 'Business',
+            backgroundColor: Constants.backgroundColor,
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.school),
+            label: 'School',
+            backgroundColor: Constants.backgroundColor,
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.settings),
+            label: 'Settings',
+            backgroundColor: Constants.backgroundColor,
+          ),
+        ],
+        currentIndex: _selectedIndex,
+        selectedItemColor: Constants.primaryColor,
+        onTap: _onItemTapped,
+      ),
+    );
   }
 }
